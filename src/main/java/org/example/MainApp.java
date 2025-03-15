64be11974d38bb1e7502c2a44acf7f5fa5d4325a
Domingo <domingojosenavarroorihuela@alumno.ieselrincon.es>
2025-03-15 13:14:00 +0000

    Quito stylo pocho. Añado un max de ancho y largo para q

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -2,11 +2,13 @@ package org.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MainApp extends Application {
 
@@ -16,8 +18,16 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Pokédex");
+        //codigo para cambiar el icono de la pokedex
+        InputStream iconStream = getClass().getResourceAsStream("/PokeBall_37565.png");
+        primaryStage.getIcons().add(new Image(iconStream));
+        //anchoLargo fijo
+        primaryStage.setMaxWidth(880);
+        primaryStage.setMaxHeight(620);
 
+
+        //
+        this.primaryStage.setTitle("Pokédex");
         initRootLayout();
     }
 
