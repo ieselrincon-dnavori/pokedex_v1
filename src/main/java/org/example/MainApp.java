b3f96e897d9506958edd02f090995ebcb314e268
ieselrincon-dnavori <domingojosenavarroorihuela@alumno.ieselrincon.es>
2025-03-17 18:33:04 +0000

    Update MainApp.java
    quitando, espacios en blanco

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -6,7 +6,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.io.InputStream;
 
